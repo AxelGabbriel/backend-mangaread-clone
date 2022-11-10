c758abfe6438a36d1d73955e4413e09716145ca1
jairoalperez <jairoalberto98@hotmail.com>
2022-11-10 00:00:10 -0500

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,10 @@
-const express= require('express')
+const express = require('express')
 const router = express.Router()
 const usuario = require('../controllers/usuario')
 //const room= require('../controllers/room')
 //const puntaje=require('../controllers/puntaje')
 //const passport=require('passport')
-const  {passportAuth}  = require('../middlewares')
+const { passportAuth } = require('../middlewares')
 
 
 
@@ -12,9 +12,9 @@ const  {passportAuth}  = require('../middlewares')
 
 
 //registro y login
-router.post('/registro',usuario.register)
+router.post('/registro', usuario.register)
 router.post('/login', passportAuth)
-router.get('/perfil',(req,res)=>{
+router.get('/perfil', (req, res) => {
     res.send('perfil')
 })
 
@@ -26,23 +26,32 @@ const sharp = require('sharp')
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')){
+    if (file.mimetype.startsWith('image')) {
         cb(null, true)
-    }else {
+    } else {
         cb('invalid image file', false)
     }
 }
-const uploads = multer({storage, fileFilter})
+const uploads = multer({ storage, fileFilter })
 
 
 
 
-router.post('/upload',  uploads.single('manga'), async (req, res) => {
-    
-    const fotoBuffer = req.file.buffer
-    const imageInfo = await sharp(fotoBuffer).metadata()
-    console.log(imageInfo)
-    res.send('ok')
+router.post('/upload', uploads.single('manga'), async (req, res) => {
+
+    try {
+        const fotoBuffer = req.file.buffer
+        const { width, height } = await sharp(fotoBuffer).metadata()
+        finalImage = await sharp(fotoBuffer.resize(343, 801)).toBuffer
+        res.status(201).json({success: true, message: 'Foto Cargada'})
+
+    } catch (error) {
+        res.status(500).json({success: false, message: 'Foto no Cargada'})
+        console.log('Error con la imagen')
+    }
+
+
+
 
 
 })
@@ -54,8 +63,8 @@ router.post('/upload',  uploads.single('manga'), async (req, res) => {
 
 
 //rutas usuario
-router.get('/buscar-usuario/:id_usuario',usuario.buscarid)
-router.get('/buscar-nombre/:username',usuario.buscarnombre)
+router.get('/buscar-usuario/:id_usuario', usuario.buscarid)
+router.get('/buscar-nombre/:username', usuario.buscarnombre)
 
 
 module.exports = router
