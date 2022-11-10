b91eb7286eff00740a3b56b7f2e20ac7ab148a4c
jairoalperez <jairoalberto98@hotmail.com>
2022-11-10 00:56:55 -0500

    Update uploadImage.js

--- src/controllers/uploadImage.js
+++ src/controllers/uploadImage.js
@@ -5,27 +5,20 @@ exports.uploadImage = async (req, res) => {
 
     console.log(req.file)
 
-    const result = await cloudinary.uploader.upload(req.file.path, {
-        public_id: `${new Date().getTime()}_manga`,
-        width: 343,
-        height: 801,
-        crop: 'fill'
-    })
-
-    console.log(result)
-
-    /*try {
-        const fotoBuffer = req.file.buffer
-        const { width, height } = await sharp(fotoBuffer).metadata()
-        finalImage = await sharp(fotoBuffer).resize(343, 801).toBuffer
+    try {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+            public_id: `${new Date().getTime()}_manga`,
+            width: 343,
+            height: 801,
+            crop: 'fill'
+        })
+        console.log(result)
         res.status(201).json({success: true, message: 'Foto Cargada'})
-
-        console.log(finalImage)
 
     } catch (error) {
         res.status(500).json({success: false, message: 'Foto no Cargada'})
         console.log('Error con la imagen', error.message)
-    }*/
+    }
 
 
 
