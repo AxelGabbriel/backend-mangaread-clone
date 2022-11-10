ccdaa7680b812e8f116e7f86be76c9d32c2ea821
jairoalperez <jairoalberto98@hotmail.com>
2022-11-10 00:03:44 -0500

    Update router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -42,12 +42,12 @@ router.post('/upload', uploads.single('manga'), async (req, res) => {
     try {
         const fotoBuffer = req.file.buffer
         const { width, height } = await sharp(fotoBuffer).metadata()
-        finalImage = await sharp(fotoBuffer.resize(343, 801)).toBuffer
+        finalImage = await sharp(fotoBuffer).resize(343, 801).toBuffer
         res.status(201).json({success: true, message: 'Foto Cargada'})
 
     } catch (error) {
         res.status(500).json({success: false, message: 'Foto no Cargada'})
-        console.log('Error con la imagen')
+        console.log('Error con la imagen', error.message)
     }
 
 
