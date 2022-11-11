053ef9d164d5527d73d995413935d0fc0aa364b4
AxelGabbriel <axelmiquilena@gmail.com>
2022-11-11 19:54:31 -0400

    rutas

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express= require('express')
 const router = express.Router()
 const usuario = require('../controllers/usuario')
-//const room= require('../controllers/room')
+const foto= require('../controllers/foto')
 //const puntaje=require('../controllers/puntaje')
 //const passport=require('passport')
-const { passportAuth } = require('../middlewares')
+const  {passportAuth}  = require('../middlewares')
 
 
 
@@ -12,35 +12,19 @@ const { passportAuth } = require('../middlewares')
 
 
 //registro y login
-router.post('/registro', usuario.register)
+router.post('/registro',usuario.register)
 router.post('/login', passportAuth)
-router.get('/perfil', (req, res) => {
+router.get('/perfil',(req,res)=>{
     res.send('perfil')
 })
-
-
-//Manejo de archivos
-const multer = require('multer')
-const { uploadImage } = require('../controllers/uploadImage')
-
-const storage = multer.diskStorage({});
-
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype.startsWith('image')) {
-        cb(null, true)
-    } else {
-        cb('invalid image file', false)
-    }
-}
-const uploads = multer({ storage, fileFilter })
-
-router.post('/upload', uploads.single('manga'), uploadImage)
-
-
-
 //rutas usuario
-router.get('/buscar-usuario/:id_usuario', usuario.buscarid)
-router.get('/buscar-nombre/:username', usuario.buscarnombre)
+router.get('/buscar-usuario/:id_usuario',usuario.buscarid)
+router.get('/buscar-nombre/:username',usuario.buscarnombre)
+//rutas para foto
+router.post('/crear-foto',foto.crear)
+router.get('/buscar-manga/:manga',foto.buscar)
+router.delete('/borrar-manga/:manga',foto.borrarmanga)
+router.delete('/borrar-capitulo/:capitulo',foto.borrarcapitulo)
 
 
 module.exports = router
