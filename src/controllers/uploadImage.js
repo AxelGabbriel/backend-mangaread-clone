a08a627fa5d79b3b11cc2aab30390107246e8b0f
jairoalperez <jairoalberto98@hotmail.com>
2022-11-12 01:34:26 -0500

    Update uploadImage.js

--- src/controllers/uploadImage.js
+++ src/controllers/uploadImage.js
@@ -6,7 +6,7 @@ exports.uploadImage = async (req, res) => {
     //console.log(req.body)
     //console.log(req.file)
 
-    const {nombre, autor, capitulo} = req.body
+    const { nombre, autor, capitulo } = req.body
 
     try {
         const result = await cloudinary.uploader.upload(req.file.path, {
@@ -15,11 +15,18 @@ exports.uploadImage = async (req, res) => {
             height: 801,
             crop: 'fill'
         })
-        console.log('url: '+result.url+', nombre: '+nombre+', autor: '+autor+', capitulo: '+capitulo)
-        res.status(201).json({success: true, message: 'Foto Cargada'})
+        console.log('url: ' + result.url + ', nombre: ' + nombre + ', autor: ' + autor + ', capitulo: ' + capitulo)
+        const resultdb = await pool.query('INSERT INTO foto(url,manga,autor,capitulo) VALUES($1,$2,$3,$4)', [
+            result.url, nombre, autor, capitulo])
+        console.log('registro exitoso')
+        res.json('registro exitoso' + resultdb.rows)
+
+
+
+        res.status(201).json({ success: true, message: 'Foto Cargada' })
 
     } catch (error) {
-        res.status(500).json({success: false, message: 'Foto no Cargada'})
+        res.status(500).json({ success: false, message: 'Foto no Cargada' })
         console.log('Error con la imagen', error.message)
     }
 
