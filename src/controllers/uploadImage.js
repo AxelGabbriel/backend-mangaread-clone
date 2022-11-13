ed7190a21fe57dcb2c32aca64ca144321806caec
jairoalperez <jairoalberto98@hotmail.com>
2022-11-12 19:12:05 -0500

    Update uploadImage.js

--- src/controllers/uploadImage.js
+++ src/controllers/uploadImage.js
@@ -26,10 +26,10 @@ exports.uploadImage = async (req, res) => {
             crop: 'fill'
         })
         console.log('url: ' + result.url + ', nombre: ' + nombre + ', autor: ' + autor + ', capitulo: ' + capitulo + ', pagina: ' + pagina)
-        //const resultdb = await pool.query('INSERT INTO foto(url,manga,autor,capitulo,image_id,pagina) VALUES($1,$2,$3,$4,$5,$6)', [
-        //    result.url, nombre, autor, capitulo, result.public_id, pagina])
+        const resultdb = await pool.query('INSERT INTO foto(url,manga,autor,capitulo,image_id,pagina) VALUES($1,$2,$3,$4,$5,$6)', [
+            result.url, nombre, autor, capitulo, result.public_id, pagina])
         console.log('registro exitoso')
-        //res.json('registro exitoso' + resultdb.rows)
+        res.json('registro exitoso' + resultdb.rows)
 
 
 
